7f5aea2b9f68c4404730479533c0af28a5713117
Suraj Gupta <surajrocks1997@gmail.com>
2025-01-12 18:25:49 +0530

    initial commit - websocket kafka producer

---
+++ websocket-kafka-producer/src/main/java/com/chat_rooms/websocket_kafka_producer/WebsocketKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.chat_rooms.websocket_kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketKafkaProducerApplication.class, args);
+	}
+
+}
---
+++ websocket-kafka-producer/src/test/java/com/chat_rooms/websocket_kafka_producer/WebsocketKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_rooms.websocket_kafka_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
