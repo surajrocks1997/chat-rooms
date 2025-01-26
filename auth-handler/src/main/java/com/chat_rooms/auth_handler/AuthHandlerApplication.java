d416f7748f2bc9e26a2c824a2e489e6af50b4d68
Suraj Gupta <surajrocks1997@gmail.com>
2025-01-26 13:08:07 +0530

    initialized auth-handler spring project

---
+++ auth-handler/src/main/java/com/chat_rooms/auth_handler/AuthHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.chat_rooms.auth_handler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthHandlerApplication.class, args);
+	}
+
+}
---
+++ auth-handler/src/test/java/com/chat_rooms/auth_handler/AuthHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_rooms.auth_handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
