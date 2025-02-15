c6b94d1946f4355f50b69c08be91c6c0ab97abf1
Suraj Gupta <surajrocks1997@gmail.com>
2025-02-15 13:35:38 +0530

    getUser updated to send back username as well

--- websocket-kafka-producer/src/main/java/com/chat_rooms/websocket_kafka_producer/dto/UserInfoProjection.java
+++ websocket-kafka-producer/src/main/java/com/chat_rooms/websocket_kafka_producer/dto/UserInfoProjection.java
@@ -2,7 +2,11 @@ package com.chat_rooms.websocket_kafka_producer.dto;
 
 public interface UserInfoProjection {
 
-   String getFirstName();
-   String getLastName();
-   String getEmail();
+    String getFirstName();
+
+    String getLastName();
+
+    String getEmail();
+
+    String getUsername();
 }
