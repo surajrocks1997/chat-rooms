cd9c6643cf2675fed387102dfc40e6fa00cc72f6
Suraj Gupta <surajrocks1997@gmail.com>
2025-08-17 19:58:03 +0530

    small change to use full kafka consumer config with container which includes error handling as well rather than just using config bean and creating a new concurrent message listener container for kafka consumer

--- websocket-kafka-producer/src/main/java/com/chat_rooms/websocket_kafka_producer/service/KafkaConsumerService.java
+++ websocket-kafka-producer/src/main/java/com/chat_rooms/websocket_kafka_producer/service/KafkaConsumerService.java
@@ -4,7 +4,7 @@ import com.chat_rooms.websocket_kafka_producer.dto.ChatRoomMessage;
 import com.chat_rooms.websocket_kafka_producer.utility.RedisKeys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.listener.AcknowledgingMessageListener;
 import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
@@ -18,19 +18,17 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class KafkaConsumerService {
 
-    private final WebSocketSubscriberService subscriberService;
     private final JsonRedisService jsonRedisService;
-
-    private final ConsumerFactory<String, ChatRoomMessage> chatRoomEventConsumerFactory;
+    private final ConcurrentKafkaListenerContainerFactory<String, ChatRoomMessage> chatRoomEventListenerContainerFactory;
     private final Map<String, ConcurrentMessageListenerContainer<String, ChatRoomMessage>> listenerContainers = new ConcurrentHashMap<>();
 
     public void startListener(String chatRoomName) {
         listenerContainers.computeIfAbsent(chatRoomName, name -> {
-            ContainerProperties containerProps = new ContainerProperties("chat-room-topic-" + name);
+            ConcurrentMessageListenerContainer<String, ChatRoomMessage> container = chatRoomEventListenerContainerFactory.createContainer("chat-room-topic-" + name);
+            ContainerProperties containerProps = container.getContainerProperties();
             containerProps.setGroupId("chat-room-consumer-" + name);
             containerProps.setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
 
-            ConcurrentMessageListenerContainer<String, ChatRoomMessage> container = new ConcurrentMessageListenerContainer<>(chatRoomEventConsumerFactory, containerProps);
             container.setupMessageListener((AcknowledgingMessageListener<String, ChatRoomMessage>) (record, ack) -> {
                 try {
                     jsonRedisService.publish(RedisKeys.BASE + record.value().getChatRoomName().getValue(), record.value());
@@ -38,6 +36,7 @@ public class KafkaConsumerService {
 
                 } catch (Exception e) {
                     log.error("Error processing message for chat room {}: {}", chatRoomName, e.getMessage(), e);
+                    throw new RuntimeException("KafkaConsumerService: Failed to process message for chat room: " + chatRoomName, e);
                 }
             });
 
