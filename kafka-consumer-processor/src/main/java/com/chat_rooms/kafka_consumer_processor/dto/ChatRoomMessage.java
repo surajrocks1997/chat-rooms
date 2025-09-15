e240df059bd9bbbd56be228cff08f54ad82c859f
Suraj Gupta <surajrocks1997@gmail.com>
2025-09-15 20:02:25 +0530

    added correlationId in chatroomMessage dto

--- kafka-consumer-processor/src/main/java/com/chat_rooms/kafka_consumer_processor/dto/ChatRoomMessage.java
+++ kafka-consumer-processor/src/main/java/com/chat_rooms/kafka_consumer_processor/dto/ChatRoomMessage.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class ChatRoomMessage {
+    private String correlationId;
     private MessageType messageType;
     private String username;
     private String userId;
